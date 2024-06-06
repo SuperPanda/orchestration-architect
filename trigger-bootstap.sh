202ab5cd3f10cc8cb09bcab5e16fa443946df7d4
Andrew Briscoe <admin@pandasportal.net>
2024-06-06 17:15:59 +0800

    Improved Trigger bootstrap script

--- trigger-bootstap.sh
+++ trigger-bootstap.sh
@@ -3,6 +3,5 @@ if [ -d $BOOTSTRAP_WORKSPACE_PATH ]; then
   rm -rf $BOOTSTRAP_WORKSPACE_PATH
 fi
 
-ansible-playbook /root/orchestration_architect/fixtures/0.8.0/playbooks/Meta/Trigger_Bootstrap.yml -i localhost
-(cd $BOOTSTRAP_WORKSPACE_PATH && git remote add read /srv/git/orchestration_architect.git && git fetch && git checkout --detach && git add . && git commit -m 'temp commit for patch diff' && git diff read/0.8.0-pristine HEAD > patch.diff)
+(cd /root/orchestration_architect/fixtures/0.8.0/ && ansible-playbook playbooks/Meta/Setup_Environment.yml && ansible-playbook playbooks/Meta/Trigger_Bootstrap.yml -i localhost) && (cd $BOOTSTRAP_WORKSPACE_PATH && git remote add read /srv/git/orchestration_architect.git && git fetch && git checkout --detach && git add . && git commit -m 'temp commit for patch diff' && git diff read/0.8.0-pristine HEAD > patch.diff)
 
