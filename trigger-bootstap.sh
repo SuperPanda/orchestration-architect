5752f9a944f9d916f76dc2a373306e6c99408d01
Andrew Briscoe <admin@pandasportal.net>
2024-06-06 19:05:36 +0800

    Trigger Bootstrap Should now generate a patch to apply to pristine

--- trigger-bootstap.sh
+++ trigger-bootstap.sh
@@ -1,7 +1,8 @@
 BOOTSTRAP_WORKSPACE_PATH="/tmp/orchestration_architect/0.8.0/bootstrap_workspace"
 if [ -d $BOOTSTRAP_WORKSPACE_PATH ]; then
+  echo "Removing $BOOTSTRAP_WORKSPACE_PATH..."
   rm -rf $BOOTSTRAP_WORKSPACE_PATH
 fi
-
-(cd /root/orchestration_architect/fixtures/0.8.0/ && ansible-playbook playbooks/Meta/Setup_Environment.yml && ansible-playbook playbooks/Meta/Trigger_Bootstrap.yml -i localhost) && (cd $BOOTSTRAP_WORKSPACE_PATH && git remote add read /srv/git/orchestration_architect.git && git fetch && git checkout --detach && git add . && git commit -m 'temp commit for patch diff' && git diff read/0.8.0-pristine HEAD > patch.diff)
+ls /tmp/orchestration_architect/0.8.0/bootstrap_workspace
+(cd /root/orchestration_architect/fixtures/0.8.0/ && ansible-playbook playbooks/Meta/Setup_Environment.yml && ansible-playbook playbooks/Meta/Trigger_Bootstrap.yml -i localhost) && (cd $BOOTSTRAP_WORKSPACE_PATH && git checkout --detach && git add -A && git commit -m 'temp commit for patch diff' && git remote add diff-target /srv/git/orchestration_architect.git && git fetch diff-target && git diff 'diff-target/0.8.0-pristine' HEAD > patch.diff)
 
